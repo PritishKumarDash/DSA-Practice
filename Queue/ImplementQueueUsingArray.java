1837533260ebd78a1a10e56f564c9a8628eca2be
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-02-23 22:47:52 +0530

    Added ImplementQueueUsingArray.java

--- Queue/ImplementQueueUsingArray.java
+++ Queue/ImplementQueueUsingArray.java
@@ -1,1 +1,25 @@
+/*
+Problem: Implement Queue
+Approach: Array simulation
+*/
 
+class MyQueue {
+    int[] arr = new int[1000];
+    int front = 0, rear = 0;
+
+    void push(int x){
+        arr[rear++] = x;
+    }
+
+    int pop(){
+        return arr[front++];
+    }
+
+    int peek(){
+        return arr[front];
+    }
+
+    boolean empty(){
+        return front == rear;
+    }
+}

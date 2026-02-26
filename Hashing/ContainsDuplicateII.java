cd5944b120e4ff42d1e2bd1d2b947dfc8b65d455
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-02-26 06:58:05 +0530

    Added ContainsDuplicateII.java

--- Hashing/ContainsDuplicateII.java
+++ Hashing/ContainsDuplicateII.java
@@ -1,1 +1,22 @@
+/*
+Problem: Contains Duplicate II (LeetCode 219)
+Approach: HashMap (Store last index of each number)
+Time: O(n)
+Space: O(n)
+*/
 
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int diff = i - map.get(nums[i]);
+                if(diff <= k) return true;
+            }
+            map.put(nums[i], i);
+        }
+        
+        return false;
+    }
+}

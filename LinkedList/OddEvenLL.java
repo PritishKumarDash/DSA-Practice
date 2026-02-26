10011abd6cad37df599374983c04a1a05f6fd121
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-02-26 23:48:14 +0530

    Added OddEvenLL.java

--- LinkedList/OddEvenLL.java
+++ LinkedList/OddEvenLL.java
@@ -1,1 +1,27 @@
+/*
+Problem: Odd Even Linked List (LeetCode 328)
+Approach: Two pointers (Odd & Even separation, then merge)
+Time: O(n)
+Space: O(1)
+*/
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
 
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = even;
+
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}

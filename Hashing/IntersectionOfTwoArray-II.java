d97e740de3aa5b4c86526c5c9cf81bac17857827
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-02-26 22:30:58 +0530

    Added IntersectionOfTwoArray-II.java

--- Hashing/IntersectionOfTwoArray-II.java
+++ Hashing/IntersectionOfTwoArray-II.java
@@ -1,1 +1,31 @@
+/*
+Problem: Intersection of two arrays ||
+Approach: Hashmap
+TIme complexity: O(n+m)
+Space Complexity: O( min ( n , m ) )
+*/
+class Solution{
+  public int[] intersect(int[] nums1, int[] nums2){
+    if(num1.length > num2.length){
+      return intersect(num2, num1);
+    }
+    List<Integer> result = new ArrayList<>();
+    Map<Integer, Integer> map = new HashMap<>();
 
+    for(int num : nums1){
+      map.put(num, map.getOrDefault(num, 0) + 1);
+    }
+
+    for(int num : nums2){
+      if(map.containsKey(num) && map.get(num)){
+        map.put(num, map.get(num) - 1);
+        result.add(num);
+      }
+    }
+    int ans[] = new int[result.size()];
+    for(int i=0; i<result.size();i++){
+      ans[i] = map.get(i);
+    }
+    return ans;
+  }
+}

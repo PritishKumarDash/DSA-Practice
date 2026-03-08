3681acfaaf5b9ddb96743c8f5d3c28461af8c676
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-03-08 09:13:55 +0530

    Added ClosestElementToZero.java

--- Arrays/ClosestElementToZero.java
+++ Arrays/ClosestElementToZero.java
@@ -1,1 +1,16 @@
-
+// Approach: Brute Force
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution{
+  public int findClosestNumber(int nums[]){
+    int closest = nums[0];
+    for(int i = 0; i < nums.length; i++){
+      if(Math.abs(nums[i]) < Math.abs(closest)){
+        closest = nums[i];
+      }else if(Math.abs(nums[i]) == Math.closest(closest) && Math.abs(nums[i]) > closest){
+        closest = nums[i];
+      }
+    }
+    return closest;
+  }
+}

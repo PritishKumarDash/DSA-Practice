7009ef128846c0d629144c142bf90080670a8581
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-03-15 18:08:37 +0530

    Added NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java

--- Arrays/NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
+++ Arrays/NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
@@ -1,1 +1,25 @@
+// APproach: SLiding Window
+// Time complexity: 0(n)
+// Space complexity: 0(1)
+class Solution {
+    public int numOfSubarrays(int[] arr, int k, int threshold){
+      int count = 0;
+      int target = threshold * k;
+      int sum = 0;
+      for(int i = 0; i < k; i++){
+        sum += arr[i];
+      }
+      if(sum >= target){
+        count++;
+      }
+      for(int i = k; i < arr.length; i++){
+        sum += arr[i];
+        sum -= arr[i - k];
 
+        if(sum >= target){
+        count++;
+        }
+      }
+      return count;
+    }
+}

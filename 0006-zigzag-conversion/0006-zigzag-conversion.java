be6535a286bfc7361c01e0f56d310b3620217b71
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-04-05 11:48:14 +0530

    Time: 5 ms (56.51%), Space: 46.3 MB (95.25%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,8 +1,10 @@
 class Solution {
     public String convert(String s, int numRows) {
+        //input - PAYPALISHIRING 
         if(numRows == 1) return s;
 
         StringBuilder[] str = new StringBuilder[numRows];
+        //str = ["", "", ""]
 
         for(int i = 0; i < numRows; i++){
             str[i] = new StringBuilder();
@@ -12,16 +14,22 @@ class Solution {
         boolean goingDown = false;
 
         for(char c : s.toCharArray()){
-            str[currRows].append(c);
+            str[currRows].append(c); //str[0].append(c) = str = ["P", "", ""]
+            //str[1].append(c) str = ["P", "A", ""]
+            //str[2].append(c) str = [["P"], ["A"], ["Y"]]
+            //str[1].append(c) str = [["P"], ["AP"]["Y"]]
+            // str[0].append(c) str = [["PA"],["AP"],["Y"]]
 
             if(currRows == 0 || currRows == numRows - 1){
-                goingDown = !goingDown;
+                goingDown = !goingDown; // goingDown = true
+                // goingDown = false because currRows = numRows - 1
+                //goingDown = true because currRows = 0
             }
 
             if(goingDown){
-                currRows++;
+                currRows++; // 0 -> 1 -> 2. 0 -> 1
             }else{
-                currRows--;
+                currRows--; // 2 -> 1 -> 0
             }
         }
         StringBuilder result = new StringBuilder();

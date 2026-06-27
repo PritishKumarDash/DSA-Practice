d71240f8a835d762649e92e8e9422d12606e86e0
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-06-27 12:56:39 +0530

    Time: 0 ms (100%), Space: 42 MB (53.61%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,14 +3,25 @@ class Solution {
         if(n <= 2){
             return n;
         }
-        int dp[] = new int[n+1];
+        // int dp[] = new int[n+1];
 
-        dp[1] = 1;
-        dp[2] = 2;
+        // dp[1] = 1;
+        // dp[2] = 2;
+
+        // for(int state = 3; state <= n; state++){
+        //     dp[state] = dp[state - 1] + dp[state - 2];
+        // }
+        // return dp[n];
+
+        int prev1 = 1;
+        int prev2 = 2;
+        int ans = 0;
 
         for(int state = 3; state <= n; state++){
-            dp[state] = dp[state - 1] + dp[state - 2];
+            ans = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = ans;
         }
-        return dp[n];
+        return ans;
     }
 }

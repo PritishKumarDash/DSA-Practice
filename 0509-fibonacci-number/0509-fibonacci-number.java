41a3e43e2ba78bde560da3a42d74d6336d1a6ff5
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-06-27 12:42:53 +0530

    Time: 0 ms (100%), Space: 42.1 MB (32.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,24 +1,37 @@
 class Solution {
+    // public int fib(int n) {
+    //     // if(n==0) return 0;
+    //     // if(n==1) return 1;
+
+    //     // return fib(n-1) + fib(n-2);
+
+    //     int dp[] = new int[n+1];
+    //     Arrays.fill(dp, -1);
+    //     return recur(n, dp);
+    // }
+    // public int recur(int n, int dp[]){
+    //     if(n <= 1){
+    //         dp[n] = n;
+    //         return dp[n];
+    //     }
+
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+    //     dp[n] = fib(n-1) + fib(n-2);
+    //     return dp[n];
+
     public int fib(int n) {
-        // if(n==0) return 0;
-        // if(n==1) return 1;
-
-        // return fib(n-1) + fib(n-2);
-
-        int dp[] = new int[n+1];
-        Arrays.fill(dp, -1);
-        return recur(n, dp);
-    }
-    public int recur(int n, int dp[]){
         if(n <= 1){
-            dp[n] = n;
-            return dp[n];
+            return n;
         }
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
 
-        if(dp[n] != -1){
-            return dp[n];
+        for(int state = 2; state <= n; state++){
+            dp[state] = dp[state-1] + dp[state-2];
         }
-        dp[n] = fib(n-1) + fib(n-2);
         return dp[n];
     }
 }

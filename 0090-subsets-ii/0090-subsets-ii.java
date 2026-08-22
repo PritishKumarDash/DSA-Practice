d23357328d77e6398adec0190af0b4e40a4078d8
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-08-22 16:52:20 +0530

    Time: 4 ms (16.88%), Space: 45 MB (60.12%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,51 @@
+// class Solution {
+//     public List<List<Integer>> subsetsWithDup(int[] nums) {
+//         Arrays.sort(nums);
+//         List<List<Integer>> list = new ArrayList<>();
+//         solve(0, nums, list, new ArrayList<>());
+//         return list;
+//     }
+//     void solve(int i, int[] nums, List<List<Integer>> list, List<Integer> cur){
+//         if(i == nums.length){
+//             if(list.contains(new ArrayList<>(cur))){
+//                 return;
+//             }
+//             list.add(new ArrayList<>(cur));
+//             return;
+//         }
+//         cur.add(nums[i]);
+//         solve(i + 1, nums, list, cur);
+
+//         cur.remove(cur.size() - 1);
+
+//         solve(i+1, nums, list, cur);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
-        solve(0, nums, list, new ArrayList<>());
-        return list;
+        Set<List<Integer>> set = new HashSet<>();
+
+        solve(0, nums, set, new ArrayList<>());
+
+        return new ArrayList<>(set);
     }
-    void solve(int i, int[] nums, List<List<Integer>> list, List<Integer> cur){
-        if(i == nums.length){
-            if(list.contains(new ArrayList<>(cur))){
-                return;
-            }
-            list.add(new ArrayList<>(cur));
+
+    void solve(int i, int[] nums,
+               Set<List<Integer>> set,
+               List<Integer> cur) {
+
+        if (i == nums.length) {
+            set.add(new ArrayList<>(cur));
             return;
         }
+
         cur.add(nums[i]);
-        solve(i + 1, nums, list, cur);
+        solve(i + 1, nums, set, cur);
 
         cur.remove(cur.size() - 1);
 
-        solve(i+1, nums, list, cur);
+        solve(i + 1, nums, set, cur);
     }
 }

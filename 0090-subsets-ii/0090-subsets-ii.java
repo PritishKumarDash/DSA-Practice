4a4725eda27e76eb7b161f8c3895506a81029bb4
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-08-22 16:59:11 +0530

    Time: 2 ms (99.73%), Space: 45.2 MB (42.34%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,51 +1,87 @@
+// // class Solution {
+// //     public List<List<Integer>> subsetsWithDup(int[] nums) {
+// //         Arrays.sort(nums);
+// //         List<List<Integer>> list = new ArrayList<>();
+// //         solve(0, nums, list, new ArrayList<>());
+// //         return list;
+// //     }
+// //     void solve(int i, int[] nums, List<List<Integer>> list, List<Integer> cur){
+// //         if(i == nums.length){
+// //             if(list.contains(new ArrayList<>(cur))){
+// //                 return;
+// //             }
+// //             list.add(new ArrayList<>(cur));
+// //             return;
+// //         }
+// //         cur.add(nums[i]);
+// //         solve(i + 1, nums, list, cur);
+
+// //         cur.remove(cur.size() - 1);
+
+// //         solve(i+1, nums, list, cur);
+// //     }
+// // }
+
 // class Solution {
 //     public List<List<Integer>> subsetsWithDup(int[] nums) {
 //         Arrays.sort(nums);
-//         List<List<Integer>> list = new ArrayList<>();
-//         solve(0, nums, list, new ArrayList<>());
-//         return list;
+//         Set<List<Integer>> set = new HashSet<>();
+
+//         solve(0, nums, set, new ArrayList<>());
+
+//         return new ArrayList<>(set);
 //     }
-//     void solve(int i, int[] nums, List<List<Integer>> list, List<Integer> cur){
-//         if(i == nums.length){
-//             if(list.contains(new ArrayList<>(cur))){
-//                 return;
-//             }
-//             list.add(new ArrayList<>(cur));
+
+//     void solve(int i, int[] nums,
+//                Set<List<Integer>> set,
+//                List<Integer> cur) {
+
+//         if (i == nums.length) {
+//             set.add(new ArrayList<>(cur));
 //             return;
 //         }
+
 //         cur.add(nums[i]);
-//         solve(i + 1, nums, list, cur);
+//         solve(i + 1, nums, set, cur);
 
 //         cur.remove(cur.size() - 1);
 
-//         solve(i+1, nums, list, cur);
+//         solve(i + 1, nums, set, cur);
 //     }
 // }
 
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        List<List<Integer>> list = new ArrayList<>();
+
         Arrays.sort(nums);
-        Set<List<Integer>> set = new HashSet<>();
 
-        solve(0, nums, set, new ArrayList<>());
+        solve(0, nums, list, new ArrayList<>());
 
-        return new ArrayList<>(set);
+        return list;
     }
 
     void solve(int i, int[] nums,
-               Set<List<Integer>> set,
+               List<List<Integer>> list,
                List<Integer> cur) {
 
         if (i == nums.length) {
-            set.add(new ArrayList<>(cur));
+            list.add(new ArrayList<>(cur));
             return;
         }
 
         cur.add(nums[i]);
-        solve(i + 1, nums, set, cur);
+        solve(i + 1, nums, list, cur);
 
         cur.remove(cur.size() - 1);
 
-        solve(i + 1, nums, set, cur);
+        int next = i + 1;
+
+        while (next < nums.length && nums[next] == nums[i]) {
+            next++;
+        }
+
+        solve(next, nums, list, cur);
     }
 }

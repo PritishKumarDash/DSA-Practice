91d57d64113d92af669ecc74c62c6f6c3a97cb99
Pritish Kumar Dash <pritishdash27@gmail.com>
2026-08-23 16:39:49 +0530

    Time: 2 ms (95.86%), Space: 45.8 MB (51.44%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,59 @@
+// class Solution {
+//     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+//         Set<List<Integer>> set = new HashSet<>();
+//         solve(0, candidates, set, new ArrayList<>(), target);
+//         return new ArrayList<>(set);
+//     }
+//     void solve(int i, int[] arr, Set<List<Integer>> set, List<Integer> cur, int target){
+
+//         if(target == 0) {
+//             set.add(new ArrayList<>(cur));
+//             return;
+//         }
+
+//         if(i == arr.length || target < 0) return;
+
+//         cur.add(arr[i]);
+
+//         solve(i+1, arr, set, cur, target - arr[i]);
+
+//         solve(i, arr, set, cur, target - arr[i]);
+
+//         cur.remove(cur.size() - 1);
+
+//         solve(i+1, arr, set, cur, target);
+//     }
+// }
+
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Set<List<Integer>> set = new HashSet<>();
-        solve(0, candidates, set, new ArrayList<>(), target);
-        return new ArrayList<>(set);
-    }
-    void solve(int i, int[] arr, Set<List<Integer>> set, List<Integer> cur, int target){
 
-        if(target == 0) {
-            set.add(new ArrayList<>(cur));
+        List<List<Integer>> ans = new ArrayList<>();
+
+        solve(0, candidates, target, ans, new ArrayList<>());
+
+        return ans;
+    }
+
+    void solve(int i, int[] arr, int target,
+               List<List<Integer>> ans,
+               List<Integer> cur) {
+
+        if (target == 0) {
+            ans.add(new ArrayList<>(cur));
             return;
         }
 
-        if(i == arr.length || target < 0) return;
+        if (i == arr.length || target < 0) {
+            return;
+        }
 
         cur.add(arr[i]);
 
-        solve(i+1, arr, set, cur, target - arr[i]);
-
-        solve(i, arr, set, cur, target - arr[i]);
+        solve(i, arr, target - arr[i], ans, cur);
 
         cur.remove(cur.size() - 1);
-        
-        solve(i+1, arr, set, cur, target);
+
+        solve(i + 1, arr, target, ans, cur);
     }
 }
